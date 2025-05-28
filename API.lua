e17065c32c883c528554ab6f2fbd0a17de8c312c
phytonhacker <168457031+phytonhacker@users.noreply.github.com>
2025-05-28 09:59:56 +0200

    GithubLoader.lua

--- API.lua
+++ API.lua
@@ -1,8 +1,25 @@
-local API = {}
+local HttpService = game:GetService("HttpService")
 
-function API.CreateWindow(movable)
-    print("Ablak létrehozva! Mozgatható:", movable)
-    -- Itt jönne a tényleges GUI logika
+local GithubLoader = {}
+
+function GithubLoader:LoadModule(url)
+    local success, result = pcall(function()
+        return HttpService:GetAsync(url)
+    end)
+
+    if not success then
+        warn("Hiba a lekérés során: ", result)
+        return nil
+    end
+
+    local fn, err = loadstring(result)
+    if not fn then
+        warn("Hibás kód:", err)
+        return nil
+    end
+
+    local module = fn()
+    return module
 end
 
-return API
+return GithubLoader
